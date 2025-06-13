f246a78fc53c9ab9d5d252f21af67ab9f74dcd55
rtldg <rtldg@protonmail.com>
2025-06-13 14:38:33 +0000

    blah

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         if not message.author.bot and not (message.guild is None):
-            if destroy_user(self, message):
+            if await self.destroy_user(self, message):
                 return
             await asyncio.sleep(1.0)
             if secrets.randbelow(100) == 0:
